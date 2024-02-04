6034a186efe0423bf452caa531f6f952f395feb5
jonathansoriano <158002831+jonathansoriano@users.noreply.github.com>
2024-02-04 13:58:40 -0500

    Started Extra Credit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[ ] args)
+    {
+        int numberToExamine = 11;
+
+        if (numberToExamine%2 == 0) {
+            System.out.println("This number is even");
+        }
+        else {
+            System.out.println("This is an odd number");
+        }
+
+
+    }
+}
